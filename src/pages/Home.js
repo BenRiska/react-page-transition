5908ad0ccb65ccf11cfb55b5446bede321acfad5
bentaylor <ben10taylor10@hotmail.com>
2020-11-12 18:03:41 +0000

    done

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Title from "../components/Title"
+
+function Home() {
+    return (
+        <div className='inner'>
+      <Title
+        lineContent="Hi, I'm"
+        lineContent2='Benjamin'
+      />
+        <p className='other'>
+          Does project report used question death, out more rhetoric unpleasing
+          what compared both of sentinels.
+        </p>
+    </div>
+    )
+}
+
+export default Home
